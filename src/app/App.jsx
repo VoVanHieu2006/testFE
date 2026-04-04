9c35778a8f2ee2e9c6580f3927be53b6d4d4bbb2
Vo Van Hieu <vovan.hieu@example.com>
2026-04-04 17:42:32 +0700

    Them tailWind css

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>Xin chào! Đây là dự án FLUXIFY làm lại từ đầu.</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-primary underline">
+        Xin chào! Tailwind CSS đã hoạt động!
+      </h1>
     </div>
   );
 }
