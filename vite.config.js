b6cfab0c34160f958e600b77003c9057e3d43e5d
Vo Van Hieu <vovan.hieu@example.com>
2026-04-16 16:33:56 +0700

    Fix: Clean code and resolve conflicts

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-<<<<<<< HEAD
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
-=======
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
->>>>>>> 5da6e881cce55fed747c9a5e45e1f41494c3555b
